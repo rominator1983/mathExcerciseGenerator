0bc925b5cfea836205cda4708c23d31b7addf0e6
rominator1983 <13437300+rominator1983@users.noreply.github.com>
2023-11-05 09:04:41 +0100

    initial excercise

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿var random = new Random();
+
+for (var i = 0; i < 50; i++)
+{
+    var excerciseType = random.Next(100);
+    if (excerciseType < 50)
+    {
+        // Addition
+        var summand1 = random.Next(13);
+        var summand2 = random.Next(13);
+
+        Console.WriteLine($"{summand1} + {summand2} =   ");
+    }
+    else if (excerciseType < 90)
+    {
+        // Subtraction
+        var minuend = random.Next(13);
+        int subtrahend;
+        do
+        {
+            subtrahend = random.Next(13);
+        }
+        while (subtrahend > minuend);
+
+        Console.WriteLine($"{minuend} - {subtrahend} =   ");
+    }
+    else
+    {
+        // Addition with multiple summands
+        var summand1 = random.Next(7);
+        var summand2 = random.Next(6);
+        var summand3 = random.Next(6);
+
+        Console.WriteLine($"{summand1} + {summand2} + {summand3} =   ");
+
+    }
+}
